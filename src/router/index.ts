573255a60ecebeaf3de980ee14556b32eeaf3e52
Dunsin <dunsin.daramola@chronosng.com>
2024-07-26 17:04:45 +0100

    fix error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { authService } from "../services/auth.service";
+// import { authService } from "../services/auth.service";
 
 const routes = [
   {
